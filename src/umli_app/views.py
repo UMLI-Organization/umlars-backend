fd1fac6cc39ee5fe49ae3237d755a6f866a768af
filip <filip.pawlowski001@gmail.com>
2024-05-04 14:35:05 +0200

    #1 Register user implemented

--- src/umli_app/views.py
+++ src/umli_app/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from .forms import SignUpForm
+
 
 def home(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         return login_user(request)
     else:
         return render(request, 'home.html', {})
+
 
 def login_user(request: HttpRequest) -> HttpResponse:
         username = request.POST.get('username')
@@ -23,7 +26,27 @@ def login_user(request: HttpRequest) -> HttpResponse:
             messages.warning(request, 'Invalid credentials')
             return redirect('home')
 
+
 def logout_user(request: HttpRequest) -> HttpResponse:
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
+
+
+def register_user(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+
+            messages.success(request, 'You have successfully registered')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'register.html', {'form': form})
